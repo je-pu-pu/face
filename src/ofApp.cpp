52c06b9ff77cb330b4b5316cc3d7b03a4a7db413
Tadashi Ando <ando@experimental.co.jp>
2020-09-23 22:24:53 +0900

    顔で奏でる音楽。

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,7 +3,7 @@
 #include "math.h"
 
 ofApp::ofApp()
-	: volume_( 0.f, 0.f, 0.01f )
+	: volume_( 0.f, 0.f, 0.025f )
 	, speed_( 1.f, 1.f, 0.1f )
 {
 
@@ -49,8 +49,6 @@ void ofApp::draw(){
     // Draw estimated 3d pose
     // tracker.drawDebugPose();
     
-	int n = 0;
-
 	for ( auto& i : tracker.getInstances() )
 	{
 		auto nose       = i.getLandmarks().getImagePoint( 33 );
@@ -59,29 +57,13 @@ void ofApp::draw(){
 
 		auto mouse_size_ratio = ( mouse_down.y - mouse_up.y ) / i.getBoundingBox().getHeight();
 
-		const auto mouse_size_ratio_min = 0.08f;
-		const auto mouse_size_ratio_max = 0.3f;
-		
-		auto volume = ( mouse_size_ratio - mouse_size_ratio_min ) / ( mouse_size_ratio_max - mouse_size_ratio_min );
+		auto volume = ( mouse_size_ratio - mouse_size_ratio_min_ ) / ( mouse_size_ratio_max_ - mouse_size_ratio_min_ );
 		volume = std::min( 1.f, std::max( 0.f, volume ) );
 
-		ofDrawBitmapStringHighlight( "mouse up   : " + ofToString( mouse_up         ), 100, 100 );
-		ofDrawBitmapStringHighlight( "mouse down : " + ofToString( mouse_down       ), 100, 120 );
-		ofDrawBitmapStringHighlight( "mouse size : " + ofToString( mouse_size_ratio ), 100, 140 );
-		ofDrawBitmapStringHighlight( "volume     : " + ofToString( volume           ), 100, 160 );
-
-		/*
-		ofDrawBitmapStringHighlight( "pose : " + ofToString( i.getPoseMatrix().getRotate().getEuler() ), 0, 500 );
-
-		auto& pl = i.getLandmarks().getImageFeature( ofxFaceTracker2Landmarks::OUTER_MOUTH );
-
-		for ( auto& p : pl )
-		{
-			ofDrawBitmapStringHighlight( ofToString( p ), 100, n * 20 );
-
-			n++;
-		}
-		*/
+		ofDrawBitmapStringHighlight( "mouse up   : " + ofToString( mouse_up         ), 10, 200 );
+		ofDrawBitmapStringHighlight( "mouse down : " + ofToString( mouse_down       ), 10, 220 );
+		ofDrawBitmapStringHighlight( "mouse size : " + ofToString( mouse_size_ratio ), 10, 240 );
+		ofDrawBitmapStringHighlight( "volume     : " + ofToString( volume           ), 10, 260 );
 
 		const auto face_x = nose.x; // ( ( i.getBoundingBox().getLeft() + i.getBoundingBox().getRight() ) / 2.f );
 		const auto speed = ( ofGetWidth() - face_x ) / ( ofGetWidth() / 2.f );
@@ -95,20 +77,28 @@ void ofApp::draw(){
 				Tone::C6
 			};
 			*/
-
+			
 			constexpr std::array< float, 11 > tones = {
 				Tone::C4, Tone::D4, Tone::E4, Tone::G4, Tone::A4,
 				Tone::C5, Tone::D5, Tone::E5, Tone::G5, Tone::A5,
 				Tone::C6
 			};
 
+			/*
+			constexpr std::array< float, 9 > tones = {
+				Tone::C4, Tone::D4, Tone::E4, Tone::G4,
+				Tone::C5, Tone::D5, Tone::E5, Tone::G5,
+				Tone::C6
+			};
+			*/
+
 			const auto face_x_ratio = ( ( ofGetWidth() - face_x ) / ofGetWidth() - 0.5f ) * tone_scaling_ + 0.5f;
 			const auto tone_index = math::clamp< int >( static_cast< int >( face_x_ratio  * tones.size() ), 0, tones.size() - 1 );
 
 			speed_.target_value() = tones[ tone_index ] / Tone::C5;
 
-			ofDrawBitmapStringHighlight( "face x ratio : " + ofToString( face_x_ratio ) + ", " + ofToString( tone_index ), 100, 180 );
-			ofDrawBitmapStringHighlight( "tone : " + ofToString( tones[ tone_index ] ) + ", " + ofToString( tone_index ), 100, 200 );
+			ofDrawBitmapStringHighlight( "face x ratio : " + ofToString( face_x_ratio ) + ", " + ofToString( tone_index ), 10, 280 );
+			ofDrawBitmapStringHighlight( "tone : " + ofToString( tones[ tone_index ] ) + ", " + ofToString( tone_index ), 10, 300 );
 		}
 		else
 		{
@@ -116,33 +106,34 @@ void ofApp::draw(){
 		}
 
 		volume_.target_value() = volume;
-		
 
-		volume_.chase();
-		speed_.chase();
-
-		// auto volume = 1.f;
-
-		sound_player_.setSpeed( speed_.value() );
-		sound_player_.setVolume( volume_.value() );
-
-		/*
-		for ( auto& p : i.getLandmarks().getImagePoints() )
-		{
-			ofDrawBitmapStringHighlight( "( " + ofToString( p.x ) + ", " + ofToString( p.y ) + " )", 100, n * 20 );
-
-			n++;
-		}
-		*/
+		break;
 	}
 
-	ofDrawBitmapStringHighlight( ofToString( mouseX ) + ", " + ofToString( mouseY ), 200, 200 );
+	if ( volume_.value() < volume_.target_value() )
+	{
+		// volume_.fit_to_target();
+		volume_.chase();
+		volume_.chase();
+	}
+	else
+	{
+		volume_.chase();
+	}
+
+	speed_.chase();
+
+	sound_player_.setSpeed( speed_.value() );
+	sound_player_.setVolume( volume_.value() );
 
     // Draw text UI
-    ofDrawBitmapStringHighlight("Framerate : "+ofToString(ofGetFrameRate()), 10, 20);
-    ofDrawBitmapStringHighlight("Tracker thread framerate : "+ofToString(tracker.getThreadFps()), 10, 40);
-	ofDrawBitmapStringHighlight("music mode : " + ofToString( is_music_mode_ ), 10, 60);
-	ofDrawBitmapStringHighlight("tone scaling : " + ofToString( tone_scaling_ ), 10, 80 );
+    ofDrawBitmapStringHighlight( "FPS : " + ofToString( ofGetFrameRate() ), 10, 20 );
+    ofDrawBitmapStringHighlight( "Tracker thread FPS : "+ofToString( tracker.getThreadFps() ), 10, 40 );
+	ofDrawBitmapStringHighlight( "music mode : " + ofToString( is_music_mode_ ), 10, 60 );
+	ofDrawBitmapStringHighlight( "tone scaling : " + ofToString( tone_scaling_ ), 10, 80 );
+	ofDrawBitmapStringHighlight( "mouse size min : " + ofToString( mouse_size_ratio_min_ ), 10, 100 );
+	ofDrawBitmapStringHighlight( "mouse size max : " + ofToString( mouse_size_ratio_max_ ), 10, 120 );
+	// ofDrawBitmapStringHighlight( "mouse size     : " + ofToString( mouse_size_ratio_ ), 10, 140 );
     
 #ifndef __OPTIMIZE__
     ofSetColor(ofColor::red);
@@ -157,12 +148,28 @@ void ofApp::keyPressed( int key )
 	{
 		is_music_mode_ = ! is_music_mode_;
 	}
-	else if ( key == 'z' )
+	else if ( key == 'q' )
 	{
 		tone_scaling_ -= 0.1f;
 	}
-	else if ( key == 'x' )
+	else if ( key == 'w' )
 	{
 		tone_scaling_ += 0.1f;
+	}
+	else if ( key == 'a' )
+	{
+		mouse_size_ratio_min_ -= 0.001f;
+	}
+	else if ( key == 's' )
+	{
+		mouse_size_ratio_min_ += 0.001f;
+	}
+	else if ( key == 'z' )
+	{
+		mouse_size_ratio_max_ -= 0.001f;
+	}
+	else if ( key == 'x' )
+	{
+		mouse_size_ratio_max_ += 0.001f;
 	}
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -24,8 +24,11 @@ public:
 	bool is_music_mode_ = false;
 	float tone_scaling_ = 2.5f;
 
-	common::chase_value< float > volume_;
-	common::chase_value< float > speed_;
+	float mouse_size_ratio_min_ = 0.08f;	///< ボリュームが最小になる時の口の大きさ ( 顔の大きさに対する比率 )
+	float mouse_size_ratio_max_ = 0.275f;	///< ボリュームが最大になる時の口の大きさ ( 顔の大きさに対する比率 )
+
+	common::chase_value< float > volume_;	///< ボリューム
+	common::chase_value< float > speed_;	///< 再生速度
 
     ofxFaceTracker2 tracker;
     ofVideoGrabber grabber;
